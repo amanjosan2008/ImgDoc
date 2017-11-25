246dfa3325aceadcdc74a567a285bc7be35d8fd6
Amandeep Singh <amanjosan2008@gmail.com>
2017-11-25 15:02:42 +0530

    gui input button added

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Button, Label
+from tkinter import Tk, Button, Label, Entry
 import sys
 
 root = Tk()
@@ -30,8 +30,18 @@ def exit():
     print("Bye Bye!!")
     sys.exit()
 
+def callback():
+    print (h.get())
+
 header = Label(root, text="Renamer App v1.0")
 header.pack()
+
+h = Entry(root)
+h.pack()
+h.focus_set()
+
+input = Button(root, text="get", width=10, command=callback)
+input.pack()
 
 a = Button(root, text="Backup Files", command=backup)
 a.pack()
@@ -53,6 +63,8 @@ f.pack()
 
 g = Button(root, text="Delete Backups", command=delete)
 g.pack()
+
+
 
 exit = Button(root, text="Exit App", command=exit)
 exit.pack()
--- non_recursive_exec.py
+++ non_recursive_exec.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 import os, sys
 
@@ -6,5 +7,3 @@ dir = sys.argv[1]
 files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 for file in files:
     print (file)
-
-
