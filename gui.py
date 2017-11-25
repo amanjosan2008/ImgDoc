ff8fb24f7252c0da1ae9587bee9f137d5463f071
Amandeep Singh <amanjosan2008@gmail.com>
2017-11-25 13:29:07 +0530

    GUI App added

---
+++ gui.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+from tkinter import Tk, Button, Label
+import sys
+
+root = Tk()
+
+def backup():
+    print("Backup Files")
+
+def unique():
+    print("Unique Extensions")
+
+def correct():
+    print("Correct Extensions")
+
+def webpconv():
+    print("Webp Convert")
+
+def colonrep():
+    print("Replace Colon")
+
+def verify():
+    print("Verify Files")
+
+def delete():
+    print("Delete Backups")
+
+def exit():
+    print("Bye Bye!!")
+    sys.exit()
+
+header = Label(root, text="Renamer App v1.0")
+header.pack()
+
+a = Button(root, text="Backup Files", command=backup)
+a.pack()
+
+b = Button(root, text="Unique Extensions", command=unique)
+b.pack()
+
+c = Button(root, text="Correct Extensions", command=correct)
+c.pack()
+
+d = Button(root, text="Webp Convert", command=webpconv)
+d.pack()
+
+e = Button(root, text="Replace Colon", command=colonrep)
+e.pack()
+
+f = Button(root, text="Verify Files", command=verify)
+f.pack()
+
+g = Button(root, text="Delete Backups", command=delete)
+g.pack()
+
+exit = Button(root, text="Exit App", command=exit)
+exit.pack()
+
+root.mainloop()
