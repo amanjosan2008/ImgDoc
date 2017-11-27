997036f102acd50eee31e0aa74529fda5d5fc9a5
Amandeep Singh <amanjosan2008@gmail.com>
2017-11-27 23:44:03 +0530

    img files

---
+++ img_conv.py
@@ -0,0 +1,17 @@
+
+
+#version      Pillow 3.0.0
+#--- TKINTER support available
+#--- JPEG support available
+#--- ZLIB (PNG/ZIP) support available
+#--- FREETYPE2 support available
+#--- WEBP support available
+
+
+import imghdr
+from PIL import Image
+
+im = Image.open("unnamed.webp").convert("RGB")
+im.save("test.jpg","jpeg")
+
+
