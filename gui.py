e9ea620b160e79aac2beb12f25a61dab0b5f5c02
amanjosan2008 <31513195+amanjosan2008@users.noreply.github.com>
2017-11-27 22:32:20 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,60 +1,51 @@
 #!/usr/bin/env python3
 
-from tkinter import Tk, Button, Label
-import sys
+from tkinter import *
+import sys, glob, os
+
+#try:
+#    directory = sys.argv[1]
+#except:
+#    print("\nException occured: Full Directory path with images not defined.\n")
+#    sys.exit()
 
 root = Tk()
 
-def backup():
-    print("Backup Files")
+def ls():
+    for name in glob.glob(os.path.join(en.get(), '*')):
+        Label(root, text=name, width=100, anchor=W, justify=LEFT).pack()
 
-def unique():
-    print("Unique Extensions")
+def x():
+    x = 0
+    for name in glob.glob(os.path.join(en.get(), '*')):
+        x+=1
+    return x
 
-def correct():
-    print("Correct Extensions")
+def count():
+    Label(root, text="Count: " + str(x()), width=100, anchor=W, justify=LEFT).pack()
 
-def webpconv():
-    print("Webp Convert")
-
-def colonrep():
-    print("Replace Colon")
-
-def verify():
-    print("Verify Files")
-
-def delete():
-    print("Delete Backups")
 
 def exit():
-    print("Bye Bye!!")
+    print("Application closed by user !!")
     sys.exit()
 
-header = Label(root, text="Renamer App v1.0")
-header.pack()
+Label(root, text="Renamer App", font=("Times", 35)).pack()
 
-a = Button(root, text="Backup Files", command=backup)
-a.pack()
+en = Entry(root, width=30)
+en.pack()
+en.focus_set()
 
-b = Button(root, text="Unique Extensions", command=unique)
-b.pack()
 
-c = Button(root, text="Correct Extensions", command=correct)
-c.pack()
+Button(root, text="ls", width=20, command=ls).pack()
+Button(root, text="Count", width=20, command=count).pack()
+Button(root, text="Backup Files", width=20, command="backup").pack()
+Button(root, text="Unique Extensions", width=20, command="unique").pack()
+Button(root, text="Correct Extensions", width=20, command="correct").pack()
+Button(root, text="Webp Convert", width=20, command="webpconv").pack()
+Button(root, text="Replace Colon", width=20, command="colonrep").pack()
+Button(root, text="Verify Files", width=20, command="verify").pack()
+Button(root, text="Delete Backups", width=20, command="delete").pack()
+Button(root, text="Exit App", width=20, command=exit).pack()
 
-d = Button(root, text="Webp Convert", command=webpconv)
-d.pack()
-
-e = Button(root, text="Replace Colon", command=colonrep)
-e.pack()
-
-f = Button(root, text="Verify Files", command=verify)
-f.pack()
-
-g = Button(root, text="Delete Backups", command=delete)
-g.pack()
-
-exit = Button(root, text="Exit App", command=exit)
-exit.pack()
-
+root.geometry("400x600")
 root.mainloop()
