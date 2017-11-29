6633041cb01e8371b9f2644406fe51a278a1da63
Amandeep Singh <amanjosan2008@gmail.com>
2017-11-29 23:01:04 +0530

    added dup, simi, webp files

--- img_conv2.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image
-import glob, os
-
-for infile in glob.glob("*.jpg"):
-    file, ext = os.path.splitext(infile)
-    im = Image.open(infile).convert("RGB")
-    im.save(file + ".webp", "WEBP")
--- webp_conv.py
+++ webp_conv.py
@@ -13,3 +13,13 @@ from PIL import Image
 
 im = Image.open("unnamed.webp").convert("RGB")
 im.save("test.jpg","jpeg")
+
+----------------------------------------------------
+
+from PIL import Image
+import glob, os
+
+for infile in glob.glob("*.jpg"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile).convert("RGB")
+    im.save(file + ".webp", "WEBP")
