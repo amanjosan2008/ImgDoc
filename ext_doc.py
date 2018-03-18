dc43757e875265cbee14c82d32545cf845c86de6
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-03-18 15:06:44 +0530

    Update ext_doc.py

--- ext_doc.py
+++ ext_doc.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# Remove Function complete listbox
+# Find out & Report Images/files as html
+# Progress Bar - Backup Fn Underconstruction
+# Add Progress Bar to all Functions
 
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, ttk
 import sys, glob, os, re
 import tarfile
 import imghdr, shutil
@@ -16,6 +18,9 @@ from hashlib import md5
 import imagehash
 
 root = Tk()
+
+frame = Frame(root, height=800, width=700, bd=3, relief=RIDGE)
+frame.grid()
 
 def fullpath():
     if var2.get():
@@ -46,9 +51,9 @@ def validate():
 
 def browse():
     try:
-        dir = filedialog.askdirectory(parent=root, initialdir='/data/.folder/', title='Please select a directory')
+        dir = filedialog.askdirectory(parent=frame, initialdir='/data/.folder/', title='Please select a directory')
     except:
-        dir = filedialog.askdirectory(parent=root, initialdir=os.getcwd(), title='Please select a directory')
+        dir = filedialog.askdirectory(parent=frame, initialdir=os.getcwd(), title='Please select a directory')
     en.delete(0,END)
     en.insert(0,dir)
 
@@ -86,10 +91,17 @@ def backup():
         tar = tarfile.open(os.path.join("tmp/" + "backup.tar.gz"), "w:gz")
         wr = open(os.path.join("tmp/" + "file_list.txt"), "w")
         wr.write("List of files:\n\n")
+        frame.config(cursor="watch")
+        frame.update()
+        i = 0
+        j = len([name for name in os.listdir(en.get()) if os.path.isfile(os.path.join(en.get(), name))])
         for name in fullpath():
             wr.write(name + '\n')
             tar.add(name)
             lb("File backup: "+name)
+            bar['value'] = (i/j)*100
+            i += 1
+        frame.config(cursor="")
         wr.close()
         tar.close()
         lb("")
@@ -117,6 +129,8 @@ def missing():
 
 def correct():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         for file in fullpath():
             # find the correct extension
             ftype = imghdr.what(file)
@@ -155,10 +169,13 @@ def correct():
                     continue
             else:
                 lb(file+": No Extension detected, Run Missing Extensions function.")
+        frame.config(cursor="")
         lb("")
 
 def webpconv():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         for file in fullpath():
             name, ext = os.path.splitext(file)
             if ext == ".webp":
@@ -188,6 +205,7 @@ def webpconv():
             else:
                 #File is not Webp, Skipping
                 continue
+        frame.config(cursor="")
         lb("")
 
 def colonrep():
@@ -204,8 +222,7 @@ def colonrep():
                     if write():
                         shutil.move(file, file.replace(":","_"))
             else:
-                #Colon not found in name
-                lb("Colon not found in filename "+name)
+                lb("Colon not found in file: "+file.split('/')[-1])
         lb("")
 
 def verify():
@@ -234,6 +251,8 @@ def delete():
 
 def duplicate():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         x,y = [],[]
         for file in fullpath():
             afile = open(file, 'rb')
@@ -263,10 +282,13 @@ def duplicate():
                             lb("Deleted duplicate file: "+ x[j])
                         else:
                             lb(x[j]+" WILL BE deleted")
+        frame.config(cursor="")
         lb("")
 
 def similar():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         x,y,z,a = [],[],[],1
         for file in fullpath():
             try:
@@ -292,12 +314,15 @@ def similar():
                 if c[i]-y[j] < 8:
                     #print(x[j], " Factor:", c[i]-y[j])
                     lb("Dupes: "+ x[j] + " Factor: "+str(c[i]-y[j]))
+        frame.config(cursor="")
         lb("")
 
 def search():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         currdir = os.getcwd()
-        img = filedialog.askopenfilename(parent=root, initialdir=currdir, title='Please select an image')
+        img = filedialog.askopenfilename(parent=frame, initialdir=currdir, title='Please select an image')
         hash1 = imagehash.whash(PIL.Image.open(img))
         x,y = [],[]
         for file in fullpath():
@@ -308,6 +333,7 @@ def search():
             if not (img == x[i]):
                 if y[i]-hash1 < 8:
                     lb(x[i]+" Factor: "+str(y[i]-hash1))
+        frame.config(cursor="")
         lb("")
 
 def stats():
@@ -324,6 +350,8 @@ def stats():
 
 def top():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         x = {}
         for file in fullpath():
             filesize = (os.path.getsize(file))
@@ -340,10 +368,13 @@ def top():
             sizeflt = "{:.2f}".format(sizeinmb)
             lb(key+" => "+sizeflt+"MB")
             x.pop((max(x, key=x.get)))
+        frame.config(cursor="")
         lb("")
 
 def hugepng():
     if validate():
+        frame.config(cursor="watch")
+        frame.update()
         x = {}
         for file in fullpath():
             filesz = (os.path.getsize(file))
@@ -375,51 +406,52 @@ def hugepng():
             else:
                 #File is not Huge PNG, Skipping
                 continue
+        frame.config(cursor="")
         lb("")
 
 def clear():
     listbox.delete(0, END)
 
-Label(root, text="File Extension Doctor", font=("Times", 35), width=20).grid(row=0, columnspan=6)
-en = Entry(root, width=60)
+Label(frame, text="File Extension Doctor", font=("Times", 35), width=20).grid(row=0, columnspan=6)
+en = Entry(frame, width=60)
 en.grid(row=1, column=0,columnspan=4)
 en.focus_set()
 
-Button(root, text="Browse", width=20, command=browse).grid(row=1, column=3)
+Button(frame, text="Browse", width=20, command=browse).grid(row=1, column=3)
 
 var = IntVar()
-c = Checkbutton(root, text="Write Access", variable=var)
+c = Checkbutton(frame, text="Write Access", variable=var)
 c.grid(row=1, column=4, rowspan=1, columnspan=1)
 
 var2 = IntVar()
-d = Checkbutton(root, text="Recursive", variable=var2)
+d = Checkbutton(frame, text="Recursive", variable=var2)
 d.grid(row=1, column=5, rowspan=1, columnspan=1)
 
-root.grid_rowconfigure(2, minsize=20)
+frame.grid_rowconfigure(2, minsize=20)
 
-Button(root, text="Show Files", width=20, command=ls).grid(row=3, column=0)
-Button(root, text="Count of Files", width=20, command=count).grid(row=4, column=0)
-Button(root, text="Backup Files", width=20, command=backup).grid(row=5, column=0)
-Button(root, text="Missing Extensions", width=20, command=missing).grid(row=6, column=0)
-Button(root, text="Correct Extensions", width=20, command=correct).grid(row=7, column=0)
-Button(root, text="Webp Convert", width=20, command=webpconv).grid(row=8, column=0)
-Button(root, text="Replace Colon", width=20, command=colonrep).grid(row=9, column=0)
-Button(root, text="Verify Files", width=20, command=verify).grid(row=10, column=0)
-Button(root, text="Delete Backups", width=20, command=delete).grid(row=11, column=0)
-Button(root, text="Delete Duplicate", width=20, command=duplicate).grid(row=12, column=0)
-Button(root, text="Find Similar Images", width=20, command=similar).grid(row=13, column=0)
-Button(root, text="Image Search", width=20, command=search).grid(row=14, column=0)
-Button(root, text="Show Stats", width=20, command=stats).grid(row=15, column=0)
-Button(root, text="Top 10 Files", width=20, command=top).grid(row=16, column=0)
-Button(root, text="Huge PNG Convertor", width=20, command=hugepng).grid(row=17, column=0)
-Button(root, text="Clear Log Output", width=20, command=clear).grid(row=18, column=0)
-Button(root, text="Exit", width=20, command=exit).grid(row=19, column=0)
+Button(frame, text="Show Files", width=20, command=ls).grid(row=3, column=0)
+Button(frame, text="Count of Files", width=20, command=count).grid(row=4, column=0)
+Button(frame, text="Backup Files", width=20, command=backup).grid(row=5, column=0)
+Button(frame, text="Missing Extensions", width=20, command=missing).grid(row=6, column=0)
+Button(frame, text="Correct Extensions", width=20, command=correct).grid(row=7, column=0)
+Button(frame, text="Webp Convert", width=20, command=webpconv).grid(row=8, column=0)
+Button(frame, text="Replace Colon", width=20, command=colonrep).grid(row=9, column=0)
+Button(frame, text="Verify Files", width=20, command=verify).grid(row=10, column=0)
+Button(frame, text="Delete Backups", width=20, command=delete).grid(row=11, column=0)
+Button(frame, text="Delete Duplicate", width=20, command=duplicate).grid(row=12, column=0)
+Button(frame, text="Find Similar Images", width=20, command=similar).grid(row=13, column=0)
+Button(frame, text="Image Search", width=20, command=search).grid(row=14, column=0)
+Button(frame, text="Show Stats", width=20, command=stats).grid(row=15, column=0)
+Button(frame, text="Top 10 Files", width=20, command=top).grid(row=16, column=0)
+Button(frame, text="Huge PNG Convertor", width=20, command=hugepng).grid(row=17, column=0)
+Button(frame, text="Clear Log Output", width=20, command=clear).grid(row=18, column=0)
+Button(frame, text="Exit", width=20, command=exit).grid(row=19, column=0)
 
-root.grid_rowconfigure(7, minsize=20)
-root.grid_columnconfigure(1, minsize=10)
+frame.grid_rowconfigure(7, minsize=20)
+frame.grid_columnconfigure(1, minsize=10)
 
-scrollbar = Scrollbar(root, orient=VERTICAL)
-listbox = Listbox(root, height=30, width=140, yscrollcommand=scrollbar.set)
+scrollbar = Scrollbar(frame, orient=VERTICAL)
+listbox = Listbox(frame, height=30, width=140, yscrollcommand=scrollbar.set)
 listbox.xview_scroll(3, "pages")
 listbox.yview_scroll(3, "pages")
 scrollbar.config(command=listbox.yview)
@@ -428,9 +460,14 @@ listbox.grid(row=3, column=2, rowspan=17, columnspan=6)
 
 lb("Ready, Log Output: ")
 
-root.geometry("1400x700")
+# Progress Bar
+bar = ttk.Progressbar(frame, length=700)
+bar.grid(row=20,column=2, sticky=W)
+
+#root.geometry("1400x700")
 root.title("Correct extensions of multiple files")
 
 img = PhotoImage(file='icon.png')
 root.tk.call('wm', 'iconphoto', root._w, img)
 root.mainloop()
+
