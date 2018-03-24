6911405baa81d9b65e4ba6d9ec10a526a3e1ff84
Amandeep Singh <31513195+amanjosan2008@users.noreply.github.com>
2018-03-24 13:52:00 +0530

    Layout fixed, HugePNG function fixed

--- ext_doc.py
+++ ext_doc.py
@@ -2,8 +2,6 @@
 # Find out & Report Images/files as html
 # Unsupported JPG Files
 # Duplicate, Similar, Search, Top Fn add => 50% + 50% ProgBar
-# HugePNG Size Compare Function not working correctly
-# Remove Missing Ext Fn
 
 from tkinter import *
 from tkinter import filedialog, ttk
@@ -18,12 +16,25 @@ import PIL
 from itertools import islice
 from hashlib import md5
 import imagehash
+import webbrowser
 
 root = Tk()
 
+# Main Frame
 frame = Frame(root, height=800, width=700, bd=3, relief=RIDGE)
 frame.grid()
 
+# Sub Frames
+frame1 = Frame(frame, height=50, width=500, bd=3, relief=GROOVE)
+frame1.grid(row=0, column=0, columnspan=2, sticky=NW)
+
+frame2 = Frame(frame, height=300, width=100, bd=3, relief=GROOVE)
+frame2.grid(row=1, column=0, rowspan=2, sticky=N)
+
+frame3 = Frame(frame, height=300, width=400, bd=3, relief=GROOVE)
+frame3.grid(row=1, column=1, sticky=N)
+
+# Auxillary Functions
 def fullpath():
     if var2.get():
         for root,dir,fname in os.walk(en.get()):
@@ -58,6 +69,7 @@ def browse():
         dir = filedialog.askdirectory(parent=frame, initialdir=os.getcwd(), title='Please select a directory')
     en.delete(0,END)
     en.insert(0,dir)
+    count_lb()
 
 def write():
     if var.get():
@@ -87,79 +99,7 @@ def count_lb():
     lb("Count: "+str(leng))
     lb("")
 
-def backup():
-    if validate():
-        try:
-            os.mkdir("tmp")
-        except Exception:
-            pass
-        tar = tarfile.open(os.path.join("tmp/" + "backup.tar.gz"), "w:gz")
-        wr = open(os.path.join("tmp/" + "file_list.txt"), "w")
-        wr.write("List of files:\n\n")
-        frame.config(cursor="watch")
-        frame.update()
-        bar['value'] = 1
-        p = 1
-        count()
-        for name in fullpath():
-            wr.write(name + '\n')
-            tar.add(name)
-            lb("File backup: "+name)
-            bar['value'] = int(p/leng*100)
-            root.update_idletasks()
-            p += 1
-        frame.config(cursor="")
-        wr.close()
-        tar.close()
-        lb("Info: Total Files backed up: "+str(p-1))
-        lb("")
-
-#def missing():
-#    if validate():
-#        frame.config(cursor="watch")
-#        frame.update()
-#        bar['value'] = 1
-#        p = 1
-#        count()
-#        c = 0
-#        d = 0
-#        for file in fullpath():
-#            bar['value'] = int(p/leng*100)
-#            root.update_idletasks()
-#            p += 1
-#            fn, ext = os.path.splitext(file)
-#            ftype = imghdr.what(file)
-#            if ftype == None:
-#                c += 1
-#                continue
-#                #lb("Error: Unsupported file"+ file.split('/')[-1])
-#            elif ftype == "jpeg":
-#                if not ext:
-#                    newname = file +"."+ "jpg"
-#                    filechk = Path(newname)
-#                    if filechk.is_file():
-#                        lb("Error: "+file+": File already EXISTS, not overwriting: "+str(filechk))
-#                    else:
-#                        lb(file+": has no ext, Appending: jpg")
-#                        if write():
-#                            shutil.move(file, newname)
-#                            d += 1            
-#            else:
-#                newname = file +"."+ ftype
-#                if not ext:
-#                    filechk = Path(newname)
-#                    if filechk.is_file():
-#                        lb("Error: "+file+": File already EXISTS, not overwriting: "+str(filechk))
-#                    else:
-#                        lb(file+": has no ext, Appending: "+ftype)
-#                        if write():
-#                            shutil.move(file, newname)
-#                            d += 1
-#                else:
-#                    continue
-#        frame.config(cursor="")
-#        lb("Info: Total Files: "+str(leng)+ " Processed Files: "+str(d)+" Unsupported Files: "+str(c))
-
+# Main Functions
 def correct():
     if validate():
         bar['value'] = 1
@@ -219,7 +159,7 @@ def correct():
                         else:
                             d += 1
                             continue
-                            #lb("Error: "+file+": Could not determine file type.")
+                            #lb("Error: "+file+": Could not determine file type")
                 else:
                     # Correct Extension
                     continue
@@ -328,32 +268,6 @@ def colonrep():
                 #lb("Error: Colon not found in file: "+file.split('/')[-1])
         frame.config(cursor="")
         lb("Info: Total Files: "+ str(leng) +" Processed Files: "+ str(c))
-        lb("")
-
-def verify():
-    if validate():
-        try:
-            lb("Initial Count: "+str(leng))
-            x = []
-            for file in fullpath():
-                x.append(file)
-            leng2 = len(x)
-            lb("Final Count: "+str(leng2))
-            if leng == leng2:
-                lb("Info: Operation completed successfully")
-            else:
-                lb("Error: Operation failed, some files missing, Restore from backup !!!")
-        except NameError:
-            lb("Error: Count function not used before operation, cannot use this feature now")
-        lb("")
-
-def delete():
-    if validate():
-        try:
-            shutil.rmtree("tmp")
-            lb("Info: Backup Directory Deleted")
-        except:
-            lb("Error: Backup not found")
         lb("")
 
 def duplicate():
@@ -480,6 +394,107 @@ def search():
         lb("Info: Total Files: "+ str(leng)+" Duplicate Sets: "+ str(c))
         lb("")
 
+def hugepng():
+    if validate():
+        frame.config(cursor="watch")
+        frame.update()
+        bar['value'] = 1
+        p,c,d = 1,0,0
+        count()
+        x = {}
+        for file in fullpath():
+            bar['value'] = int(p/leng*100)
+            root.update_idletasks()
+            p += 1
+            filesz = os.path.getsize(file)
+            name, ext = os.path.splitext(file)
+            # search huge PNG Files
+            if ((ext == ".png")|(ext == ".PNG")) & (filesz > 1000000):
+                d += 1
+                fnpng = name + ".jpg"
+                fpath = Path(fnpng)
+                if fpath.is_file():
+                    lb("Error: "+file+"("+filesize(file)+"MB)"+ ": File already EXISTS, not overwritting: "+ fnpng+ "("+filesize(fnpng)+"MB)" )
+                else:
+                    try:
+                        if write():
+                            im = Image.open(file).convert("RGB")
+                            im.save(fnpng, "jpeg")
+                            if fpath.is_file():
+                                lb(file+"("+filesize(file)+"MB)"+ " deleted and Converted file saved as: "+ fnpng+ "("+filesize(fnpng)+"MB)")
+                                os.remove(file)
+                                c += 1
+                            else:
+                                lb("Error: "+file+": Conversion to JPG failed")
+                        else:
+                            lb("Info: "+file+"("+filesize(file)+"MB)"+" be deleted and Converted file be saved as: "+fnpng)
+                    except OSError as e:
+                        lb("Error: "+file+": Exception occured: "+str(e))
+                        pass
+                    except:
+                        lb("Error: Exception error occured when processing: "+file)
+                        pass
+            else:
+                #File is not Huge PNG, Skipping
+                continue
+        frame.config(cursor="")
+        lb("Info: Total Files: "+ str(leng) + " Huge PNG Files: "+ str(d)+ " Converted Files: "+str(c))
+        lb("")
+
+# More Auxillary Functions
+def backup():
+    if validate():
+        try:
+            os.mkdir("tmp")
+        except Exception:
+            pass
+        tar = tarfile.open(os.path.join("tmp/" + "backup.tar.gz"), "w:gz")
+        wr = open(os.path.join("tmp/" + "file_list.txt"), "w")
+        wr.write("List of files:\n\n")
+        frame.config(cursor="watch")
+        frame.update()
+        bar['value'] = 1
+        p = 1
+        count()
+        for name in fullpath():
+            wr.write(name + '\n')
+            tar.add(name)
+            lb("File backup: "+name)
+            bar['value'] = int(p/leng*100)
+            root.update_idletasks()
+            p += 1
+        frame.config(cursor="")
+        wr.close()
+        tar.close()
+        lb("Info: Total Files backed up: "+str(p-1))
+        lb("")
+
+def verify():
+    if validate():
+        try:
+            lb("Initial Count: "+str(leng))
+            x = []
+            for file in fullpath():
+                x.append(file)
+            leng2 = len(x)
+            lb("Final Count: "+str(leng2))
+            if leng == leng2:
+                lb("Info: Operation completed successfully")
+            else:
+                lb("Error: Operation failed, some files missing, Restore from backup !!!")
+        except NameError:
+            lb("Error: Count function not used before operation, cannot use this feature now")
+        lb("")
+
+def delete():
+    if validate():
+        try:
+            shutil.rmtree("tmp")
+            lb("Info: Backup Directory Deleted")
+        except:
+            lb("Error: Backup not found")
+        lb("")
+
 def stats():
     if validate():
         lb("Directory Stats are below:")
@@ -519,118 +534,101 @@ def top():
             lb(key+" ("+sizeflt+" MB)")
             x.pop((max(x, key=x.get)))
         frame.config(cursor="")
-        #lb("Info: Total Files: "+ str(leng))
-        lb("")
-
-def hugepng():
-    if validate():
-        frame.config(cursor="watch")
-        frame.update()
-        bar['value'] = 1
-        p = 1
-        count()
-        x = {}
-        c = 0
-        for file in fullpath():
-            bar['value'] = int(p/leng*100)
-            root.update_idletasks()
-            p += 1
-            filesz = os.path.getsize(file)
-            name, ext = os.path.splitext(file)
-            # search huge PNG Files
-            #print(filesz/1000000)
-            if (ext == ".png")|(ext == ".PNG") & (filesz > 1000000):
-                #print("H: ",filesz/1000000)
-                fnpng = name + ".jpg"
-                fpath = Path(fnpng)
-                if fpath.is_file():
-                    lb("Error: "+file+"("+filesize(file)+"MB)"+ ": File already EXISTS, not overwritting: "+ fnpng+ "("+filesize(fnpng)+"MB)" )
-                else:
-                    try:
-                        if write():
-                            im = Image.open(file).convert("RGB")
-                            im.save(fnpng, "jpeg")
-                            if fpath.is_file():
-                                lb(file+"("+filesize(file)+"MB)"+ " deleted and Converted file saved as: "+ fnpng+ "("+filesize(fnpng)+"MB)")
-                                os.remove(file)
-                                c += 1
-                            else:
-                                lb("Error: "+file+": Conversion to JPG failed")
-                        else:
-                            lb("Info: "+file+"("+filesize(file)+"MB)"+" be deleted and Converted file be saved as: "+fnpng)
-                    except OSError as e:
-                        lb("Error: "+file+": Exception occured: "+str(e))
-                        pass
-                    except:
-                        lb("Error: Exception error occured when processing: "+file)
-                        pass
-            else:
-                #File is not Huge PNG, Skipping
-                continue
-        frame.config(cursor="")
-        lb("Info: Total Files: "+ str(leng) + " Converted Files: "+str(c))
         lb("")
 
 def clear():
     listbox.delete(0, END)
 
-Label(frame, text="File Extension Doctor", font=("Times", 35), width=20).grid(row=0, columnspan=6)
+def page():
+    webbrowser.open_new(r"https://github.com/amanjosan2008/Video-Collection-Manager")
 
-en = Entry(frame, width=60)
-en.grid(row=1, column=1)
+def about():
+    win1 = Toplevel()
+    win1.attributes('-topmost','true')
+    win1.title("About")
+
+    frame4 = Frame(win1, height=100, width=300, bd=3, relief=GROOVE)
+    frame4.grid()
+    Button(frame4, text="Visit Project Page", command=page).grid(row=0, sticky=W)
+
+# Menu Configuration
+menu = Menu(frame)
+
+item1 = Menu(menu, tearoff=0)
+item1.add_command(label='Browse', command=browse)
+item1.add_separator()
+item1.add_command(label='Exit', command=exit)
+
+item2 = Menu(menu, tearoff=0)
+item2.add_command(label='File Count', command=count_lb)
+item2.add_command(label='List Files', command=ls)
+item2.add_command(label='Top 10 Files', command=top)
+item2.add_separator()
+item2.add_command(label='Show Stats', command=stats)
+item2.add_command(label='Verify Files', command=verify)
+
+item3 = Menu(menu, tearoff=0)
+item3.add_command(label='Backup Files', command=backup)
+item3.add_command(label='Delete Backup', command=delete)
+
+item4 = Menu(menu, tearoff=0)
+item4.add_command(label='Clear Logs', command=clear)
+item4.add_command(label='About', command=about)
+
+menu.add_cascade(label='File', menu=item1)
+menu.add_cascade(label='Info', menu=item2)
+menu.add_cascade(label='Options', menu=item3)
+menu.add_cascade(label='Help', menu=item4)
+
+root.config(menu=menu)
+
+# Entry Widget
+Label(frame1, text="Directory: ", width=15).grid(row=0, column=0)
+en = Entry(frame1, width=60)
+en.grid(row=0, column=1)
 en.focus_set()
 
-Button(frame, text="Browse", width=15, command=browse).grid(row=1, column=2)
-
+# Checkbuttons
 var = IntVar()
-c = Checkbutton(frame, text="Write Access", variable=var)
-c.grid(row=1, column=3, rowspan=1, columnspan=1)
+c = Checkbutton(frame1, text="Write Access", variable=var)
+c.grid(row=0, column=2)
 
 var2 = IntVar()
-d = Checkbutton(frame, text="Recursive", variable=var2)
-d.grid(row=1, column=4, rowspan=1, columnspan=1)
+d = Checkbutton(frame1, text="Recursive", variable=var2)
+d.grid(row=0, column=3)
 
-frame.grid_rowconfigure(2, minsize=20)
+# Buttons for Main Functions
+Button(frame2, text="Correct Extensions", width=12, command=correct).grid(row=0)
+Button(frame2, text="Webp Convert", width=12, command=webpconv).grid(row=1)
+Button(frame2, text="Replace Colon", width=12, command=colonrep).grid(row=2)
+Button(frame2, text="Duplicates", width=12, command=duplicate).grid(row=3)
+Button(frame2, text="Similar Images", width=12, command=similar).grid(row=4)
+Button(frame2, text="Image Search", width=12, command=search).grid(row=5)
+Button(frame2, text="Huge PNG", width=12, command=hugepng).grid(row=6)
 
-Button(frame, text="Show Files", width=20, command=ls).grid(row=3, column=0)
-Button(frame, text="File Count", width=20, command=count_lb).grid(row=4, column=0)
-Button(frame, text="Show Stats", width=20, command=stats).grid(row=5, column=0)
-Button(frame, text="Top 10 Files", width=20, command=top).grid(row=6, column=0)
-Button(frame, text="Backup Files", width=20, command=backup).grid(row=7, column=0)
-Button(frame, text="Delete Backup", width=20, command=delete).grid(row=8, column=0)
-Button(frame, text="Verify Files", width=20, command=verify).grid(row=9, column=0)
-#Button(frame, text="Missing Extensions", width=20, command=missing).grid(row=10, column=0)
-Button(frame, text="Correct Extensions", width=20, command=correct).grid(row=10, column=0)
-Button(frame, text="Webp Convert", width=20, command=webpconv).grid(row=11, column=0)
-Button(frame, text="Replace Colon", width=20, command=colonrep).grid(row=12, column=0)
-Button(frame, text="Duplicates", width=20, command=duplicate).grid(row=13, column=0)
-Button(frame, text="Similar Images", width=20, command=similar).grid(row=14, column=0)
-Button(frame, text="Image Search", width=20, command=search).grid(row=15, column=0)
-Button(frame, text="Huge PNG", width=20, command=hugepng).grid(row=16, column=0)
-Button(frame, text="Clear Logs", width=20, command=clear).grid(row=17, column=0)
-Button(frame, text="Exit", width=20, command=exit).grid(row=18, column=0)
-
-frame.grid_rowconfigure(7, minsize=20)
-frame.grid_columnconfigure(1, minsize=10)
-
-scrollbar = Scrollbar(frame, orient=VERTICAL)
-listbox = Listbox(frame, height=30, width=140, yscrollcommand=scrollbar.set)
+# Listbox & Scrollbar
+scrollbar = Scrollbar(frame3, orient=VERTICAL)
+listbox = Listbox(frame3, height=30, width=100, yscrollcommand=scrollbar.set)
 listbox.xview_scroll(3, "pages")
 listbox.yview_scroll(3, "pages")
+listbox.grid(row=0)
 scrollbar.config(command=listbox.yview)
-scrollbar.grid(row=3, column=8, rowspan=17, columnspan=1, ipady = 226)
-listbox.grid(row=3, column=1, rowspan=17, columnspan=6)
+scrollbar.grid(row=0, column=1, ipady = 226)
 
 lb("Ready, Log Output: ")
 
 # Progress Bar
-bar = ttk.Progressbar(frame, length=700)
-bar.grid(row=20, column=1, columnspan=2)
+bar = ttk.Progressbar(frame3, length=700)
+bar.grid(row=1, column=0)
 
-root.title("Correct extensions of multiple files")
+root.title("File Extension Doctor")
 
-img = PhotoImage(file='icon.png')
-root.tk.call('wm', 'iconphoto', root._w, img)
+try:
+    img = PhotoImage(file='icon.png')
+    root.tk.call('wm', 'iconphoto', root._w, img)
+except:
+    lb("Error: icon.png file not found")
+
 root.mainloop()
 
 
